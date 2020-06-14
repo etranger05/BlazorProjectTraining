0d200ed235f97dab60da9b624f011cf8e0a3783a
etranger05 <etranger05@gmail.com>
2020-06-14 05:00:45 +0100

    add portfolio website menu compoenent  and event handler

---
+++ BlazorAppTraining/Pages/Portfolios/PortfolioWebsite.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppTraining.Pages.Portfolios
+{
+    public partial class PortfolioWebsite : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManagerRef { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected void GotoMyProfile()
+        {
+            //NavigationManagerRef.NavigateTo("http://profile.tracywith.me");
+            NavigationManagerRef.NavigateTo("/index");
+
+        }
+    }
+}
