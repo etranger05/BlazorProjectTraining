e027e967937f355bfebc6137106d7e7fe955c6a7
etranger05 <etranger05@gmail.com>
2020-06-16 00:50:28 +0100

    Added In-Memory Todo List Demo Bootstrap and Razor syntaxed Tab demo

---
+++ BlazorAppModels/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppModels
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool isdone { get; set; }
+    }
+}
