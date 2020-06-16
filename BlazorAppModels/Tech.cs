eb84b26c223e5e758f149e799535645e6ca47c78
etranger05 <etranger05@gmail.com>
2020-06-16 17:16:18 +0100

    Add Calendar Component Example Add Fontawesome

---
+++ BlazorAppModels/Tech.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorAppModels
+{
+    public class Tech
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
--- BlazorAppTraining/Pages/Index.razor.cs
+++ BlazorAppTraining/Pages/Index.razor.cs
@@ -1,11 +1,22 @@
-﻿namespace BlazorApp.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
 {
     public partial class Index
     {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
         protected override void OnInitialized()
         {
             base.OnInitialized();
+        }
 
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JSRuntime.InvokeAsync<object>("RunCarousel");
         }
     }
 }
